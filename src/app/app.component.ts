be02441ee2f8b822e90254b0ac4061a9ce135398
Douglas Gabriel <douglasdm97@gmail.com>
2018-04-14 15:34:12 -0300

    Changes  new Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform,ModalController} from 'ionic-angular';
+import { Nav, NavController, Platform,ModalController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -16,13 +16,13 @@ export class MyApp {
 
   rootPage: any;
 
-  constructor(public modalCtrl: ModalController, public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public navCtrl: NavController,public modalCtrl: ModalController, public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     let modal = this.modalCtrl.create(ModalPage);
 
     modal.onDidDismiss(data => {
-      this.rootPage = GamePage;
+      this.navCtrl.setRoot(GamePage);
     });
     modal.present();
 
